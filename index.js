429577f878e9493f0283a14272d82ceb820ccdac
Robert Niimi <robertn702@gmail.com>
2016-09-19 22:03:42 -0700

    fixes typo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dispatchStatus: require('./lib/dispatch_status.js'),
+  dispatchStatus: require('./lib/dispatch_statuses.js'),
   dispatchWrapper: require('./lib/dispatch_wrapper.js')
 }
